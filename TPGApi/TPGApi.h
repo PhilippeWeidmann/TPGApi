0188e31cd5f31862c0f0d99d463db460bb3e8f5b
Philippe Weidmann <philweidmann@me.com>
2018-11-05 09:22:27 +0100

    Initial Commit

---
+++ TPGApi/TPGApi.h
@@ -0,0 +1,19 @@
+//
+//  TPGApi.h
+//  TPGApi
+//
+//  Created by Philippe Weidmann on 05.11.18.
+//  Copyright © 2018 Philippe Weidmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TPGApi.
+FOUNDATION_EXPORT double TPGApiVersionNumber;
+
+//! Project version string for TPGApi.
+FOUNDATION_EXPORT const unsigned char TPGApiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TPGApi/PublicHeader.h>
+
+
